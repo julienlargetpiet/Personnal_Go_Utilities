6c30f8c6e79b980e1ebf51823e5b49d5bdfa83e3
Julien <pgyygp090@gmail.com>
2025-05-04 22:46:37 +0200

    a

--- ffmpeg_overlay.go
+++ ffmpeg_overlay.go
@@ -6,7 +6,7 @@ import (
   "os/exec"
 )
 
-var first_file_audio bool = true
+var first_file_audio bool = false
 var audio_codec string = "aac"
 var video_codec string = "mp4"
 var command_val string
@@ -51,6 +51,18 @@ func main() {
     fmt.Println(err)
     return
   }
+  _, err = os.Stat("overlay_file." + video_codec)
+  if err == nil {
+    fmt.Println("Removing overlay_file." + video_codec + "...")
+    err = os.Remove("overlay_file." + video_codec)
+    if err != nil {
+      fmt.Println(err)
+      return
+    }
+  } else if !os.IsNotExist(err) {
+    fmt.Println(err)
+    return
+  }
   _, err = os.Stat("out." + video_codec)
   if err == nil {
     fmt.Println("Removing out." + video_codec + "...")
@@ -72,7 +84,16 @@ func main() {
     return
   }
   duration_vid = string(out)
-  fmt.Println("Duration of background video in seconds: ", duration_vid, " seconds")
+  duration_vid = duration_vid[0:len(duration_vid) - 1]
+  fmt.Println("Duration of background video in seconds: ", duration_vid)
+
+  fmt.Println("Cutting overlay file...")
+  command_val = "ffmpeg -i " + ovrl_file + " -c copy -ss 0 -t " + duration_vid + " overlay_file." + video_codec
+  fmt.Println(command_val)
+  cmd := exec.Command("sh", "-c", command_val)
+  cmd.Start()
+  cmd.Wait()
+
   command_val = `ffprobe -i ` + ovrl_file + ` -show_entries format=duration -v quiet -of csv="p=0"`
   out, err = exec.Command("sh", "-c", command_val).Output()
   if err != nil {
@@ -81,17 +102,17 @@ func main() {
   }
   duration_vid = string(out)
   fmt.Println("Duration of overlay video in seconds: ", duration_vid)
-
+ 
   if first_file_audio {
     command_val = "ffmpeg -i " + base_file + " -vn -acodec copy audio." + audio_codec
   } else {
-    command_val = "ffmpeg -i " + ovrl_file + " -vn -acodec copy audio." + audio_codec
+    command_val = "ffmpeg -i overlay_file." + video_codec + " -vn -acodec copy audio." + audio_codec
   }
   fmt.Println("Extracting Audio from video...")
-  cmd := exec.Command("sh", "-c", command_val)
+  cmd = exec.Command("sh", "-c", command_val)
   cmd.Start()
   cmd.Wait()
-  command_val = `ffmpeg -i ` + base_file + ` -i ` + ovrl_file + ` -filter_complex "[1:v]scale=` + width + `:` + height + `[ovrl];[0:v][ovrl]overlay=x=(main_w-overlay_w):y=570[outv]" -map "[outv]" -shortest raw_overlayed.` + video_codec
+  command_val = `ffmpeg -i ` + base_file + ` -i overlay_file.` + video_codec + ` -filter_complex "[1:v]scale=` + width + `:` + height + `[ovrl];[0:v][ovrl]overlay=x=(main_w-overlay_w):y=570[outv]" -map "[outv]" -shortest raw_overlayed.` + video_codec
   fmt.Println("Overlaying...")
   cmd = exec.Command("sh", "-c", command_val)
   cmd.Start()
