081f2636efa23c4250c457c8ff52e334aaebb9cf
Julien <pgyygp090@gmail.com>
2025-05-04 15:15:55 +0200

    updated client_download file to dwnl

--- client_download.go
+++ client_download.go
@@ -26,7 +26,7 @@ func client_downloader(url *string, created_file *string) error {
 }
 
 func main() {
-  url :="http://0.0.0.0:8080/download/1.jpg_Lucas_40-212-169-92-204-122-186-29-63-46-160-58-168-24-195-250-" //do not forget https if TLS on server side
+  url :="http://0.0.0.0:8080/download/file_you_want" //do not forget https if TLS on server side
   created_file := "downloaded.jpg"
   err := client_downloader(&url, &created_file)
   if err != nil {
