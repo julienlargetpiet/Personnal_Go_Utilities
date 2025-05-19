5b7600d729a01b1a97efbc136f78143d5d94e5e6
Julien <pgyygp090@gmail.com>
2025-05-19 11:24:18 +0200

    added ByteSliceToInt and updated IntToByteSlice to handle 256 and lower

--- Int_and_Bytes/main.go
+++ Int_and_Bytes/main.go
@@ -5,30 +5,55 @@ import (
 )
 
 func IntToByteSlice(x int) []byte {
+  if x == 256 {
+    return []byte{0, 0}
+  } else if x < 256 {
+    return []byte{byte(x)}
+  }
   var rtn_byte []byte
   var rest int = x % 256
   rtn_byte = append(rtn_byte, byte(rest))
-  fmt.Println("rest:", rest)
   x -= rest
-  fmt.Println(x)
   x /= 256
-  fmt.Println(x)
   for x > 256 {
     rtn_byte = append(rtn_byte, 255)
     rest = x % 256
     rtn_byte = append(rtn_byte, byte(rest))
     x -= rest
     x /= 256
-    fmt.Println(x)
   }
   rtn_byte = append(rtn_byte, byte(x - 1))
   return rtn_byte
 }
 
-
+func ByteSliceToInt(x *[]byte) int {
+  var rtn_int int = 256
+  var ref_mult int = 256
+  var i int = len(*x) - 1
+  if i == 0 {
+    return int((*x)[0])
+  }
+  for i > -1 {
+    rtn_int = ((int((*x)[i]) + 1) * ref_mult + int((*x)[i - 1]))
+    ref_mult = rtn_int
+    i -= 2
+  }
+  return rtn_int
+}
 
 func main() {
-  var x int = 102564558
+  var x int = 172564558
   x2 := IntToByteSlice(x)
   fmt.Println(x2)
+  x = 271
+  x2 = IntToByteSlice(x)
+  fmt.Println(x2)
+  x = ByteSliceToInt(&x2)
+  fmt.Println(x)
+  x2 = []byte{15}
+  x = ByteSliceToInt(&x2)
+  fmt.Println(x)
 }
+
+
+
